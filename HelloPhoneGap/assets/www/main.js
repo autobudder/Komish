d27675753c4008929dd87bc1b894fb55c3c58973
Kartik <kar2905@gmail.com>
2012-05-30 23:45:48 +0530

    adding database support, inserting birthdays

--- HelloPhoneGap/assets/www/main.js
+++ HelloPhoneGap/assets/www/main.js
@@ -2,6 +2,7 @@ if ((typeof cordova == 'undefined') && (typeof Cordova == 'undefined')) alert('C
 if (typeof CDV == 'undefined') alert('CDV variable does not exist. Check that you have included cdv-plugin-fb-connect.js correctly');
 if (typeof FB == 'undefined') alert('FB variable does not exist. Check that you have included the Facebook JS SDK file.');
 var x = [];
+var db;
 FB.Event.subscribe('auth.login', function(response) {
 //	alert('auth.login event');
 });
@@ -56,6 +57,54 @@ function onError(contactError) {
 alert('onError!');
 }
 
+// Populate the database 
+//
+function populateDB(tx) {
+     //tx.executeSql('DROP TABLE IF EXISTS DEMO');
+   //  tx.executeSql('CREATE TABLE IF NOT EXISTS DEMO (id unique, data)');
+    for(var i = 0; i< x.length; i++){
+      var sql = 'INSERT INTO birthdays (uid,name,birthday) VALUES (' + x[i].id + ',"' + x[i].name + '","' + x[i].bday + '")';
+    	      tx.executeSql(sql);
+    	      console.log("sql:" + sql);
+    	
+    }
+     //tx.executeSql('INSERT INTO DEMO (id, data) VALUES (2, "Second row")');
+}
+
+// Transaction error callback
+//
+function errorCB(tx, err) {
+    alert("Error processing SQL: "+ print(arguments) + err);
+}
+   function queryDB(tx) {
+        tx.executeSql('SELECT * FROM birthdays', [], querySuccess, errorCB);
+    }
+
+    // Query the success callback
+    //
+    function querySuccess(tx, results) {
+        // this will be empty since no rows were inserted.
+        //console.log("Insert ID = " + results.insertId);
+        // this will be 0 since it is a select statement
+        console.log("Rows Affected = " + results.rowAffected);
+        // the number of rows returned by the select statement
+        console.log("Insert ID = " + results.rows.length);
+        //console.log("db dump:" + print(results));
+         var len = results.rows.length;
+        for (var i=0; i<len; i++){
+        $("#log").append("Row = " + i + " " + results.rows.item(i).uid + " " + results.rows.item(i).name + " " + results.rows.item(i).birthday);
+  		  }
+        return true;
+    }
+    
+// Transaction success callback
+//
+function successCB() {
+    alert("success!");
+    db.transaction(queryDB, errorCB);
+    return true;
+}
+    
 function getLoginStatus() {
 	FB.getLoginStatus(function(response) {
 					  if (response.status == 'connected') {
@@ -95,10 +144,12 @@ function me() {
 			if(item.hasOwnProperty("birthday") ){
 				console.log("bday:" + item.birthday);
 				console.log("name:" + item.name);
-        		x.push({name: item.name, bday: item.birthday});
+				console.log("json:" + print(item));
+        		x.push({id: item.id, name: item.name, bday: item.birthday});
       }
     }
     console.log("array:" + print(x));
+    db.transaction(populateDB, errorCB, successCB);
     search(x[0].name, function(contacts){
 				console.log("found:"+ print(contacts));
         
@@ -141,7 +192,7 @@ FB.login(
  if (response.session) {
  alert('logged in');
  } else {
- alert('not logged in');
+ //alert('not logged in');
  }
  },
  { scope: "email,friends_birthday" }
@@ -150,7 +201,7 @@ FB.login(
 
 document.addEventListener('deviceready', function() {
   try {
-  alert('Device is ready! Make sure you set your app_id below this alert.');
+ // alert('Device is ready! Make sure you set your app_id below this alert.');
   FB.init({ appId: "405651006145953", nativeInterface: CDV.FB, useCachedDialogs: false });
   document.getElementById('data').innerHTML = "";
   } catch (e) {
@@ -171,4 +222,11 @@ function messageSend(x){
 	);
 						
 }
-//document.addEventListener("deviceready", onDeviceReady, false);
+function createTable(tx){
+  tx.executeSql('CREATE TABLE IF NOT EXISTS birthdays (uid primary key, name text, birthday text)');
+}
+function onDeviceReady(){
+  db = window.openDatabase("autobudder", "1.0", "AutoBudder", 1000000);
+  db.transaction(createTable, errorCB, successCB);
+}
+document.addEventListener("deviceready", onDeviceReady, false);
